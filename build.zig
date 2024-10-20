1a903d6e790fef1124f54352c4232731ba3374f6
Gandalf-Le-Dev <matproz.gaming@gmail.com>
2024-10-20 22:54:25 +0200

    fix: fixed build file to use static lib

--- build.zig
+++ build.zig
@@ -10,13 +10,22 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const zigbeam_lib = b.addStaticLibrary(.{
+        .name = "zigbeam",
+        .root_source_file = b.path("src/zigbeam.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(zigbeam_lib);
+
     // Add test step
     const main_tests = b.addTest(.{
         .root_source_file = b.path("src/zigbeam.zig"),
         .target = target,
         .optimize = optimize,
     });
-
+    b.installArtifact(main_tests);
+    main_tests.linkLibrary(zigbeam_lib);
     const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
